88f1b1c5d98e62b3155c1d5793dfd7ec305ec79c
vanpipka <vanpipka@gmail.com>
2019-07-30 23:13:29 +0300

    binary search

--- binary_search.py
+++ binary_search.py
@@ -17,5 +17,5 @@ def binary_search(list, item):
 
 my_list = [1, 3, 5, 7, 9]
 
-print binary_search(my_list, 3)  # =>1
-print binary_search(my_list, -1)  # =>None
+print(binary_search(my_list, 3))  # =>1
+print(binary_search(my_list, -1))  # =>None
